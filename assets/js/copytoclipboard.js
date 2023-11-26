8155a847d48b549404a2132eb062a1dbcfd206c9
Jeann Andrade <jeann.andrade@outlook.com>
2023-11-26 16:07:49 -0300

    mudando formato do sc ript js

---
+++ assets/js/copytoclipboard.js
@@ -0,0 +1,20 @@
+function loadCopyButton() {
+  // get all <code> elements
+  var allCodeBlocksElements = $("code");
+
+  allCodeBlocksElements.each(function(i) {
+    // add different id for each code block
+
+    // target
+    var currentId = "codeblock" + (i + 1);
+    $(this).attr('id', currentId);
+
+    //trigger
+    var clipButton = '<button class="btn" data-clipboard-target="#' + currentId + '"><img src="https://clipboardjs.com/assets/images/clippy.svg" width="13" alt="Copy to clipboard"></button>';
+      $(this).after(clipButton);
+  });
+
+  new ClipboardJS('.btn');
+}
+
+
